0299439dab5b725699a9931b7af897c3f8c3e701
dasosann <goodtony0403@gmail.com>
2025-10-22 00:01:49 +0900

    feat: LogoHeader 컴포넌트 추가

---
+++ src/feature/global/components/LogoHeader.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface LogoHeaderProps {
+  showLogin?: boolean;
+}
+
+export default function LogoHeader({ showLogin = true }: LogoHeaderProps) {
+  return (
+    <div className='fixed top-0 left-0 right-0 flex justify-between px-5 h-18 items-center bg-white'>
+      <Image
+        src='/assets/login/cheese-logo.svg'
+        width={120}
+        height={120}
+        alt='치즈 아이콘'
+      />
+      {showLogin && (
+        <Link href='/login'>
+          <div className='py-[10px] px-3 cursor-pointer'>
+            <span className='text-body-sm-medium text-text-basic'>로그인</span>
+          </div>
+        </Link>
+      )}
+    </div>
+  );
+}
