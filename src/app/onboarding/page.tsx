eaf4906c50a9513154a833768a1c719ccc8f3372
dasosann <goodtony0403@gmail.com>
2025-10-22 00:03:40 +0900

    feat: 온보딩 페이지 컴포넌트 추가

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,10 @@
+import LogoHeader from '@/feature/global/components/LogoHeader';
+
+export default function OnBoarding() {
+  return (
+    <div className='flex flex-col'>
+      <LogoHeader showLogin={false} />
+      <div>ㅇㅇ</div>
+    </div>
+  );
+}
