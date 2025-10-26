2d83a0a6536f18a55ae2b100810d9d19ada54f83
caseBread <kgu0515@gmail.com>
2025-10-26 17:50:01 +0900

    feat : 프로파일 포매팅 적용

--- src/feature/main/components/profile/ProfileMypage.tsx
+++ src/feature/main/components/profile/ProfileMypage.tsx
@@ -4,9 +4,9 @@ interface ProfileMypageProps {}
 
 export default function ProfileMypage({}: ProfileMypageProps) {
   return (
-    <section className='mx-auto max-w-[430px] px-5 py-4'>
+    <section className='px-5 py-4'>
       <div className='flex items-center gap-4'>
-        <div className='flex w-20 items-center justify-center overflow-hidden rounded-full'>
+        <div className='flex items-center justify-center overflow-hidden rounded-full'>
           <Image
             src='/assets/onboarding/smile1.svg'
             alt='프로필사진'
@@ -18,7 +18,7 @@ export default function ProfileMypage({}: ProfileMypageProps) {
 
         <div className='flex-1'>
           <div className='mb-2'>
-            <span className='text-body-lg-semibold text-gray-900'>제리</span>
+            <span className='text-body-lg-semibold text-text-basic'>제리</span>
           </div>
 
           <div className='grid grid-cols-3 gap-6'>
@@ -26,7 +26,7 @@ export default function ProfileMypage({}: ProfileMypageProps) {
               <span className='text-heading-sm-semibold text-text-basic'>
                 21
               </span>
-              <span className='text-body-sm-regular text-gray-500'>
+              <span className='text-body-sm-regular text-text-subtle'>
                 앨범 수
               </span>
             </div>
@@ -34,7 +34,7 @@ export default function ProfileMypage({}: ProfileMypageProps) {
               <span className='text-text-basic text-heading-sm-semibold'>
                 203
               </span>
-              <span className='text-body-sm-regular text-gray-500'>
+              <span className='text-body-sm-regular text-text-subtle'>
                 올린 사진
               </span>
             </div>
@@ -42,7 +42,7 @@ export default function ProfileMypage({}: ProfileMypageProps) {
               <span className='text-heading-sm-semibold text-text-basic'>
                 441
               </span>
-              <span className='text-body-sm-regular text-gray-500'>
+              <span className='text-body-sm-regular text-text-subtle'>
                 받은 띱
               </span>
             </div>
