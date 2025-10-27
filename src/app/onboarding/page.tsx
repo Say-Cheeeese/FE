302b83e89fb51b866d5ff04f67c89eabccb955d5
dasosann <goodtony0403@gmail.com>
2025-10-27 15:18:18 +0900

    feat: Spinner 컴포넌트 추가 및 OnBoardingClient에서 Suspense로 감싸기

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,9 +1,10 @@
-import { Suspense } from 'react';
 import OnBoardingClient from '@/feature/onboarding/components/OnBoardingClient';
+import Spinner from '@/global/components/Spinner';
+import { Suspense } from 'react';
 
 export default function OnBoardingPage() {
   return (
-    <Suspense>
+    <Suspense fallback={<Spinner />}>
       <OnBoardingClient />
     </Suspense>
   );
