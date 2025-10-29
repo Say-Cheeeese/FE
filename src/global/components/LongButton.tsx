78265b1a78959471e33b1db6f81839db0a22c819
dasosann <goodtony0403@gmail.com>
2025-10-30 01:18:18 +0900

    feat: LongButton 컴포넌트에 상태 관리 및 스타일링 추가

--- src/global/components/LongButton.tsx
+++ src/global/components/LongButton.tsx
@@ -1,12 +1,33 @@
+import React from 'react';
+
 interface LongButtonProps {
   text: string;
+  disabled?: boolean;
+  onClick?: () => void;
+  sideGap?: number; // 좌우 여백(px)
 }
 
-export default function LongButton({ text }: LongButtonProps) {
+export default function LongButton({
+  text,
+  disabled = false,
+  onClick,
+  sideGap = 16,
+}: LongButtonProps) {
+  const sideGapStyle = {
+    left: sideGap,
+    right: sideGap,
+  };
   return (
     <button
       type='button'
-      className='bg-button-primary-fill text-body-1xl-semibold text-text-primary relative w-full rounded-lg py-[15px] text-center'
+      disabled={disabled}
+      onClick={onClick}
+      style={sideGapStyle}
+      className={`text-body-1xl-semibold fixed bottom-[calc(20px+env(safe-area-inset-bottom))] z-50 mx-auto h-14 max-w-[430px] rounded-lg transition-colors duration-100 ${
+        disabled
+          ? 'bg-button-disabled-fill text-text-disabled cursor-not-allowed'
+          : 'bg-button-primary-fill text-text-primary active:bg-button-primary-fill-pressed active:text-text-primary'
+      } `}
     >
       {text}
     </button>
