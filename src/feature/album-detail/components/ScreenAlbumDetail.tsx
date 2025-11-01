d0cabf36c6e5b7e3e2d7e0e4b43db86521c112c2
caseBread <kgu0515@gmail.com>
2025-11-01 14:06:34 +0900

    feat : 바텀시트 추가  (임시)

--- src/feature/album-detail/components/ScreenAlbumDetail.tsx
+++ src/feature/album-detail/components/ScreenAlbumDetail.tsx
@@ -1,7 +1,15 @@
+import BottomSheetModal from '@/global/components/modal/BottomSheetModal';
+
 interface ScreenAlbumDetailProps {
   albumId: string;
 }
 
 export default function ScreenAlbumDetail({ albumId }: ScreenAlbumDetailProps) {
-  return <></>;
+  return (
+    <>
+      <BottomSheetModal trigger={<button>btn</button>} showHandle={false}>
+        123123
+      </BottomSheetModal>
+    </>
+  );
 }
