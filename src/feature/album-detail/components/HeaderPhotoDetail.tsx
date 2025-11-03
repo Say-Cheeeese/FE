efe5a596b2f55417b783121e59a1f4239ac5758d
caseBread <kgu0515@gmail.com>
2025-11-04 05:27:05 +0900

    docs : 주석추가

--- src/feature/album-detail/components/HeaderPhotoDetail.tsx
+++ src/feature/album-detail/components/HeaderPhotoDetail.tsx
@@ -3,9 +3,13 @@ import { X } from 'lucide-react';
 interface HeaderPhotoDetailProps {}
 
 export default function HeaderPhotoDetail({}: HeaderPhotoDetailProps) {
+  const handleClose = (): void => {
+    // TODO : X버튼 클릭로직 구현
+  };
+
   return (
     <section className='flex items-center justify-between gap-3 p-5'>
-      <div className='h-8 w-8 flex-shrink-0'>
+      <div className='h-8 w-8 shrink-0'>
         <img
           src='/assets/onboarding/smile1.svg'
           alt='프로필사진'
@@ -17,7 +21,7 @@ export default function HeaderPhotoDetail({}: HeaderPhotoDetailProps) {
       <span className='typo-heading-sm-semibold text-text-basic-inverse flex-1 truncate'>
         김수한무거북이와두루
       </span>
-      <button type='button'>
+      <button type='button' onClick={handleClose}>
         <X width='24' height='24' color='white' />
       </button>
     </section>
