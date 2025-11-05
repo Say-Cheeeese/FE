1243a671d4f92b16d9cfc2213cda72d2d7f9dc97
caseBread <kgu0515@gmail.com>
2025-11-05 19:25:57 +0900

    feat : maze script 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
+import Script from 'next/script';
 import './globals.css';
 
 const pretendard = localFont({
@@ -21,6 +22,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
+      <Script id='maze-snippet' strategy='afterInteractive'>
+        {`(function (m, a, z, e) {
+  var s, t, u, v;
+  try {
+    t = m.sessionStorage.getItem('maze-us');
+  } catch (err) {}
+  if (!t) {
+    t = new Date().getTime();
+    try {
+      m.sessionStorage.setItem('maze-us', t);
+    } catch (err) {}
+  }
+  u = document.currentScript || (function () {
+    var w = document.getElementsByTagName('script');
+    return w[w.length - 1];
+  })();
+  v = u && u.nonce;
+  s = a.createElement('script');
+  s.src = z + '?apiKey=' + e;
+  s.async = true;
+  if (v) s.setAttribute('nonce', v);
+  a.getElementsByTagName('head')[0].appendChild(s);
+  m.mazeUniversalSnippetApiKey = e;
+})(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '65a6039c-2384-4c2d-a8e1-546a4f859a08');`}
+      </Script>
+
       <body
         className={`${pretendard.variable} antialiased`}
         style={{
