5a43092aee389ed4a8bdd160dc1fa8d4de729468
caseBread <kgu0515@gmail.com>
2025-11-05 23:50:06 +0900

    feat : header추가

--- src/feature/album/detail/components/ScreenAlbumDetail.tsx
+++ src/feature/album/detail/components/ScreenAlbumDetail.tsx
@@ -1,5 +1,29 @@
+import CustomHeader from '@/global/components/header/CustomHeader';
+import { ArrowDownUp, Menu } from 'lucide-react';
+
 interface ScreenAlbumDetailProps {}
 
 export default function ScreenAlbumDetail({}: ScreenAlbumDetailProps) {
-  return <></>;
+  return (
+    <>
+      <CustomHeader
+        isShowBack
+        title=''
+        rightContent={
+          <div className='flex gap-4'>
+            <button type='button'>
+              <ArrowDownUp
+                width={24}
+                height={24}
+                color='var(--color-icon-basic)'
+              />
+            </button>
+            <button type='button'>
+              <Menu width={24} height={24} color='var(--color-icon-basic)' />
+            </button>
+          </div>
+        }
+      />
+    </>
+  );
 }
