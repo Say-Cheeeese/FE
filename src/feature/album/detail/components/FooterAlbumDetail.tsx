9e69b1e1fc924240598072675d4ecd065b13d63d
caseBread <kgu0515@gmail.com>
2025-11-07 05:50:04 +0900

    feat : handle함수연동

--- src/feature/album/detail/components/FooterAlbumDetail.tsx
+++ src/feature/album/detail/components/FooterAlbumDetail.tsx
@@ -10,14 +10,18 @@ type AlbumType = 'all' | 'deep';
 export default function FooterAlbumDetail({}: FooterAlbumDetailProps) {
   const [albumType, setAlbumType] = useState<AlbumType>('all');
 
+  const handlePhotoAdd = () => {};
+
   const handleToggleChange = (value: AlbumType) => {
     setAlbumType(value);
   };
+  const handlePhotoDownload = () => {};
 
   return (
     <section className='fixed bottom-0 flex w-full items-center justify-between gap-3 bg-[linear-gradient(180deg,rgba(24,25,27,0)_0%,rgba(24,25,27,0.8)_60.1%)] px-4 py-5'>
       <button
         type='button'
+        onClick={handlePhotoAdd}
         className='bg-element-gray-light rounded-full p-2.5'
       >
         <Plus width={24} height={24} color={'var(--color-icon-basic)'} />
@@ -29,6 +33,7 @@ export default function FooterAlbumDetail({}: FooterAlbumDetailProps) {
       />
       <button
         type='button'
+        onClick={handlePhotoDownload}
         className='bg-element-gray-light rounded-full p-2.5'
       >
         <Download width={24} height={24} color={'var(--color-icon-basic)'} />
