4ddf7b87dfed38a1365c244d2b8c56d290aa5a46
caseBread <kgu0515@gmail.com>
2025-11-13 18:43:29 +0900

    refactor : 화살표 -> function

--- src/feature/photo-detail/hooks/useAlbumPhotosQuery.ts
+++ src/feature/photo-detail/hooks/useAlbumPhotosQuery.ts
@@ -2,23 +2,23 @@ import { ApiReturns, EP } from '@/global/api/ep';
 import { api } from '@/global/utils/api';
 import { useQuery } from '@tanstack/react-query';
 
-const getData = async (code: string) => {
+async function getData(code: string) {
   const data = api.get<ApiReturns['album.photos']>({
     path: EP.album.photos(code),
   });
 
   return data;
-};
+}
 
 interface UseAlbumPhotosQueryProps {
   code: string;
 }
 
-export const useAlbumPhotosQuery = ({ code }: UseAlbumPhotosQueryProps) => {
+export function useAlbumPhotosQuery({ code }: UseAlbumPhotosQueryProps) {
   const query = useQuery({
     queryKey: [EP.album.photos(code)],
     queryFn: () => getData(code),
   });
 
   return query;
-};
+}
