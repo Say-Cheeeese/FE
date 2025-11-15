4da0dc41a5286b7eab42b8adc568acff38d2661d
caseBread <kgu0515@gmail.com>
2025-11-15 23:59:27 +0900

    feat : 편지 내용 추가

--- src/feature/album-entry/components/FullSizeLetter.tsx
+++ src/feature/album-entry/components/FullSizeLetter.tsx
@@ -1,11 +1,20 @@
 'use client';
+import { useGetAlbumInvitation } from '@/feature/album/detail/hooks/useGetAlbumInvitation';
+import { convertUnicodeToEmoji } from '@/global/utils/convertEmoji';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-interface FullSizeLetterProps {}
+interface FullSizeLetterProps {
+  albumId: string;
+}
 
-export default function FullSizeLetter({}: FullSizeLetterProps) {
+export default function FullSizeLetter({ albumId }: FullSizeLetterProps) {
   const router = useRouter();
+
+  const { data, isPending, isError } = useGetAlbumInvitation(albumId);
+
+  if (isPending) return null;
+  if (isError) return null;
 
   return (
     <>
@@ -13,10 +22,10 @@ export default function FullSizeLetter({}: FullSizeLetterProps) {
       <div className='border-border-primary-lighter relative z-10 mx-9 mt-20 rounded-[20px] border bg-white pb-100 shadow-[0_12px_40px_rgba(0,0,0,0.08)]'>
         <header className='border-border-gray-lighter flex items-center gap-2 border-b px-5 py-5'>
           <Image
-            src={`/assets/onboarding/smile1.svg`}
+            src={data?.makerProfileImage ?? '/icon/error-image.svg'}
             width={32}
             height={32}
-            alt='프로필 사진'
+            alt={data?.makerName ?? '프로필 사진'}
             className='rounded-full'
           />
           <span className='typo-body-lg-semibold text-text-subtler'>맹쏘</span>
@@ -24,23 +33,28 @@ export default function FullSizeLetter({}: FullSizeLetterProps) {
 
         <section className='flex flex-col items-center py-8'>
           <div className='bg-element-gray-light mb-4 flex h-20 w-20 items-center justify-center rounded-full text-3xl'>
-            <span>🍔</span>
+            <span>
+              {data?.themeEmoji
+                ? convertUnicodeToEmoji(data?.themeEmoji)
+                : '😀'}
+            </span>
           </div>
 
           <h2 className='typo-heading-sm-semibold text-text-basic text-center'>
-            김수한무거북이와두루미삼천갑자
+            {data?.title}
           </h2>
 
           <p className='typo-body-sm-regular text-text-subtler pt-1'>
-            2025.08.23
+            {data?.eventDate}
           </p>
 
           <span className='typo-caption-sm-medium text-text-basic-inverse bg-element-primary mt-3 inline-flex items-center rounded-full px-2.5 py-1'>
+            {/* TODO : 삭제까지 얼마나 남았는지 백엔드로부터 받아야함. */}
             앨범 소멸까지 2시간 5분
           </span>
 
           <button
-            onClick={() => router.push('/photo-share-entry')}
+            onClick={() => router.push(`/album/upload/${albumId}`)}
             type='button'
             className='bg-button-primary-fill typo-body-lg-semibold text-text-inverse mt-8 w-[230px] rounded-[14px] px-6 py-3'
           >
