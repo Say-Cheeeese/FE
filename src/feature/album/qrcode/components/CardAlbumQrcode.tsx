791d548b6b18cb7f4faadb5686dc21514fd35daa
caseBread <kgu0515@gmail.com>
2025-11-15 16:01:03 +0900

    feat : QR API 연동

--- src/feature/album/qrcode/components/CardAlbumQrcode.tsx
+++ src/feature/album/qrcode/components/CardAlbumQrcode.tsx
@@ -1,21 +1,31 @@
+import { convertUnicodeToEmoji } from '@/global/utils/convertEmoji';
 import QRCode from 'react-qr-code';
+import { useGetAlbumInfo } from '../../detail/hooks/useGetAlbumInfo';
 
 interface CardAlbumQrcodeProps {
   albumId: string;
 }
 
 export default function CardAlbumQrcode({ albumId }: CardAlbumQrcodeProps) {
+  const { data, isPending, isError } = useGetAlbumInfo(albumId);
+
+  if (isPending) return null;
+  if (isError) return null;
+  if (!data) return null;
+
   const qrValue = `${process.env.NEXT_PUBLIC_CLIENT_URL}/album/entry/${albumId}`;
 
   return (
     <section className='flex flex-col items-center rounded-[20px] bg-white px-10 py-8 text-center shadow-[0px_10px_30px_rgba(98,78,44,0.08)]'>
       <div className='bg-element-gray-lighter mb-3 flex h-16 w-16 items-center justify-center rounded-full text-3xl'>
-        🍔
+        {data.themeEmoji ? convertUnicodeToEmoji(data.themeEmoji) : '😀'}
       </div>
       <h1 className='typo-heading-sm-semibold text-text-basic mb-3 break-keep'>
-        김수한무거북이와두루미삼천갑자
+        {data.title}
       </h1>
-      <p className='typo-body-sm-regular text-text-subtler mb-3'>2025.08.23</p>
+      <p className='typo-body-sm-regular text-text-subtler mb-3'>
+        {data.eventDate}
+      </p>
       <div className='bg-element-gray-lighter h-[250px] w-[250px]'>
         <QRCode
           value={qrValue}
