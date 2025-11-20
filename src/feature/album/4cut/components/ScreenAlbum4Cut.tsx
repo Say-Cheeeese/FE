1ed7ae05fc77a06a46fcf473c365cdefdf9dd15b
caseBread <kgu0515@gmail.com>
2025-11-20 22:13:56 +0900

    fix : 앨범만료되면 현재TOP4사진문구없앰

--- src/feature/album/4cut/components/ScreenAlbum4Cut.tsx
+++ src/feature/album/4cut/components/ScreenAlbum4Cut.tsx
@@ -137,7 +137,9 @@ export default function ScreenAlbum4Cut({ albumId }: ScreenAlbum4CutProps) {
         }
       />
       <section className='absolute top-[46%] left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center'>
-        <div className='typo-body-lg-semibold mb-2'>현재 TOP 4 사진</div>
+        {!isFinalized && (
+          <div className='typo-body-lg-semibold mb-2'>현재 TOP 4 사진</div>
+        )}
         <div ref={captureRef}>
           <Container4Cut
             albumId={albumId}
