dad1772ef88092bf1dd348caee0eb32bab0c1203
caseBread <kgu0515@gmail.com>
2025-11-21 18:22:33 +0900

    fix : 메이커, 나 뱃지 크기 수정

--- src/feature/album/detail/sidebar/components/ItemParticipant.tsx
+++ src/feature/album/detail/sidebar/components/ItemParticipant.tsx
@@ -24,12 +24,12 @@ export default function ItemParticipant({
         <div className='typo-body-lg-semibold text-text-basic truncate'>
           {name}
           {role === 'MAKER' && (
-            <span className='typo-caption-sm-medium text-text-secondary bg-element-primary-lighter ml-2 rounded-full px-2'>
+            <span className='typo-caption-sm-medium text-text-secondary bg-element-primary-lighter ml-2 rounded-full px-2 py-0.5'>
               메이커
             </span>
           )}
           {isMe && (
-            <span className='typo-caption-sm-medium text-text-subtler bg-element-gray-light ml-2 rounded-full px-[3.5px]'>
+            <span className='typo-caption-sm-medium text-text-subtler bg-element-gray-light ml-2 rounded-full px-[3.5px] py-0.5'>
               나
             </span>
           )}
