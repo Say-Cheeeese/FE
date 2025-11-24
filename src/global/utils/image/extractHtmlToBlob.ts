3de207a050691121377c8bd0401c88cddbc1cc7c
caseBread <kgu0515@gmail.com>
2025-11-24 17:40:52 +0900

    fix : 불필요주석제거

--- src/global/utils/image/extractHtmlToBlob.ts
+++ src/global/utils/image/extractHtmlToBlob.ts
@@ -2,10 +2,8 @@ import { toBlob } from 'html-to-image';
 
 /** html요소를 받아 Blob형태 이미지로 반환 */
 export const extractHtmlToBlob = async (node: HTMLElement): Promise<Blob> => {
-  // TODO : 모바일 화질문제 개선필요
   const blob = await toBlob(node, {
     cacheBust: true,
-    pixelRatio: 5,
   });
 
   if (!blob) {
