3de6d2660e2f613f43d6a97ecd4b7cf1be91673d
dasosann <goodtony0403@gmail.com>
2025-11-25 02:07:49 +0900

    fix: qa 내용 반영

--- src/feature/root/components/ScreenRoot.tsx
+++ src/feature/root/components/ScreenRoot.tsx
@@ -58,7 +58,7 @@ export default function ScreenRoot() {
       </div>
 
       <span className='typo-body-sm-regular mt-7 mb-3 text-[#747681]'>
-        1000개 모임에서 치이이즈를 사용했어요
+        92개 모임에서 치이이즈를 사용했어요
       </span>
       <LongButton
         text='우리 앨범 만들기'
