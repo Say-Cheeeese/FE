0ce386a88c77b0c096b584dd8a9c1faa989cb4c8
caseBread <kgu0515@gmail.com>
2025-11-25 18:51:04 +0900

    fix : 최근다운로드 로직 수정

--- src/feature/photo-detail/hooks/usePhotoDownloadMutation.ts
+++ src/feature/photo-detail/hooks/usePhotoDownloadMutation.ts
@@ -1,6 +1,7 @@
 import { ApiReturns, EP } from '@/global/api/ep';
 import { api } from '@/global/utils/api';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { updateCacheAlbumPhotosDownload } from '../modules/updateCacheAlbumPhotosDownload';
 
 const fetchData = async ({
   albumId,
@@ -20,9 +21,16 @@ interface UsePhotoDownloadMutationProps {
 }
 
 export function usePhotoDownloadMutation() {
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: ({ albumId, photoIds }: UsePhotoDownloadMutationProps) =>
       fetchData({ albumId, photoIds }),
+    onSuccess: (_data, variables) => {
+      const { albumId, photoIds } = variables;
+      photoIds.forEach((photoId) =>
+        updateCacheAlbumPhotosDownload({ albumId, photoId, queryClient }),
+      );
+    },
   });
 
   return mutation;
