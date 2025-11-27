1847307cba0cc9e6c8ce3530abde254e3b262aa7
dasosann <goodtony0403@gmail.com>
2025-11-28 01:35:59 +0900

    fix: 뒤로가기 시 main으로 이동

--- src/app/create-album/page.tsx
+++ src/app/create-album/page.tsx
@@ -1,11 +1,18 @@
 'use client';
 import CreateAlbumList from '@/feature/create-album/components/CreateAlbumList';
 import CustomHeader from '@/global/components/header/CustomHeader';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
+  const router = useRouter();
+
   return (
     <div>
-      <CustomHeader title='앨범 만들기' isShowBack={true} />
+      <CustomHeader
+        title='앨범 만들기'
+        isShowBack={true}
+        onBackClick={() => router.push('/main')}
+      />
       <CreateAlbumList />
     </div>
   );
