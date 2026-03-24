933b891257073314d99bca4903e4307d6ac8fb59
caseBread <kgu0515@gmail.com>
2026-03-24 20:35:38 +0900

    fix : 빌드에러수정

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import * as React from 'react';
-import * as SwitchPrimitive from '@radix-ui/react-switch';
 import { cn } from '@/lib/utils';
+import * as SwitchPrimitive from '@radix-ui/react-switch';
+import * as React from 'react';
 
 function Switch({
   className,
@@ -12,7 +12,7 @@ function Switch({
     <SwitchPrimitive.Root
       data-slot='switch'
       className={cn(
-        'peer data-[state=checked]:bg-[#2C2C30] data-[state=unchecked]:bg-[#E9EBEF] focus-visible:border-ring focus-visible:ring-ring/50 inline-flex h-6 w-11 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50',
+        'peer focus-visible:border-ring focus-visible:ring-ring/50 inline-flex h-6 w-11 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-[#2C2C30] data-[state=unchecked]:bg-[#E9EBEF]',
         className,
       )}
       {...props}
